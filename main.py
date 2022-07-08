1745c0ee59f0177d3305b0b318054e7f0c46d02c
edbosisto <e.bosisto@gmail.com>
2022-07-08 12:06:03 +0200

    sigmoid and initialize_with_zeros functions created

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import h5py
+
+
+# FUNCTIONS
+
+def sigmoid(z):
+    """
+    Compute the sigmoid of z
+
+    Arguments:
+    z -- A scalar or numpy array of any size.
+
+    Return:
+    s -- sigmoid(z)
+    """
+    s = 1 / (1 + np.exp(-z))
+    return s
+
+
+def initialize_with_zeros(dim):
+    """
+    This function creates a vector of zeros of shape (dim, 1) for w and initializes b to 0.
+
+    Argument:
+    dim -- size of the w vector we want (or number of parameters in this case)
+
+    Returns:
+    w -- initialized vector of shape (dim, 1)
+    b -- initialized scalar (corresponds to the bias) of type float
+    """
+    w = np.zeros((dim, 1))
+    b = float(0)
+    return w, b
+
 
 
