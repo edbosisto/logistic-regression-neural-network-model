647da9a0df3600c5d56eb75bbd12ce2859cbcc6e
edbosisto <e.bosisto@gmail.com>
2022-07-08 12:07:21 +0200

    propagate function created

--- main.py
+++ main.py
@@ -34,4 +34,39 @@ def initialize_with_zeros(dim):
     return w, b
 
 
+def propagate(w, b, X, Y):
+    """
+    Implement the cost function and its gradient for the propagation explained above
+
+    Arguments:
+    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
+    b -- bias, a scalar
+    X -- data of size (num_px * num_px * 3, number of examples)
+    Y -- true "label" vector (containing 0 if non-cat, 1 if cat) of size (1, number of examples)
+
+    Return:
+    cost -- negative log-likelihood cost for logistic regression
+    dw -- gradient of the loss with respect to w, thus same shape as w
+    db -- gradient of the loss with respect to b, thus same shape as b
+
+    Tips:
+    - Write your code step by step for the propagation. np.log(), np.dot()
+    """
+    m = X.shape[1]
+
+    A = sigmoid(np.dot(w.T, X) + b)
+    cost = -(np.sum((1 - Y) * (np.log(1 - A)) + (Y * np.log(A)))) / m
+
+    dw = (np.dot(X, (A - Y).T)) / m
+    db = (np.sum(A - Y)) / m
+
+    cost = np.squeeze(np.array(cost))
+
+    grads = {"dw": dw,
+             "db": db}
+
+    return grads, cost
+
+
+
 
